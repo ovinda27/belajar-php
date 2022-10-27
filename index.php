e3c186bd8d05088cfbbd3088ba77be6dbffa7cbc
Ovinda Dwi Kiranasari <ovindadwi27@gmail.com>
2022-10-27 13:52:42 +0700

    tugas 8

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
-$nama = "ovinda";
 
-echo $nama;
+echo "Hello World!<br>";
 
+$nama ="Ovinda Dwi Kiranasari";
+$umur =21;
 
+echo "Saya adalah <strong>$nama, saya berusia $umur tahun.</br>";
+
+$namaAdik ="Bima Triyono";
+$umurAdik ="18";
+
+echo "Adik saya bernama $namaAdik, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih kami adalah $selisihumur tahun.";
 
 ?>
